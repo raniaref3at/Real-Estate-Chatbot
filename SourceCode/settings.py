3f5581d7f23c1d6ec7d7cc87823630c9fa98ec4a
raniaref3at <daliarefaat211@gmail.com>
2024-11-15 12:37:00 +0200

    hide api key in environment

--- SourceCode/settings.py
+++ SourceCode/settings.py
@@ -1,5 +1,6 @@
-
-api_key="7uvNPwVoyFgg2xF2LqCF3CGcvEMTVEBwz0eduSlJOz7ssb7x5hMbJQQJ99AKACF24PCXJ3w3AAABACOGKpz3"
+import os
+api_key = os.getenv("AZURE_OPENAI_API_KEY")
+#api_key=<"7uvNPwVoyFgg2xF2LqCF3CGcvEMTVEBwz0eduSlJOz7ssb7x5hMbJQQJ99AKACF24PCXJ3w3AAABACOGKpz3">
 azure_endpoint="https://assessment.openai.azure.com/"
 api_version="2024-10-21"
 model="gpt-4" 
